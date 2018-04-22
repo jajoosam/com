189719c5e20d4432485701c2a10410aa14395f3f
Glitch (com) <none>
2018-04-22 17:15:20 +0000

    📺⛲️ Updated with Glitch - Adding Voting, New landing, GUI for adding filters

---
+++ public/la.js
@@ -0,0 +1,82 @@
+// TODO
+  // LOCATION - https://eu1.locationiq.org/v1/search.php?key=9cfec2bc4ab06a&q=ahmedabad&format=json
+function las(){
+if(document.getElementById("sel").value!="user"){
+  document.getElementById("next").style.display = "none";
+}
+if(document.getElementById("sel").value=="list"){
+  document.getElementById("next").style.display = "inline";
+  document.getElementById("next").placeholder = "Enter as username/ListName"
+}
+  else{
+      document.getElementById("next").style.display = "inline";
+  }
+}
+
+function laso(){
+  if(document.getElementById("sell").value=="rt1"){document.getElementById("uno-ans").style.display = "inline";}
+  if(document.getElementById("sell").value=="fave1"){document.getElementById("uno-ans").style.display = "inline";}
+  if(document.getElementById("sell").value=="near1"){document.getElementById("uno-ans").style.display = "inline";}
+  if(document.getElementById("sell").value=="reply1"){document.getElementById("uno-ans").style.display = "inline";}
+  if(document.getElementById("sell").value=="ment1"){document.getElementById("uno-ans").style.display = "inline";}
+}
+
+function lasu(){
+  if(document.getElementById("selll").value=="rt2"){document.getElementById("dos-ans").style.display = "inline";}
+  if(document.getElementById("selll").value=="reply2"){document.getElementById("dos-ans").style.display = "inline";}
+  if(document.getElementById("selll").value=="fave2"){document.getElementById("dos-ans").style.display = "inline";}
+  if(document.getElementById("selll").value=="near2"){document.getElementById("dos-ans").style.display = "inline";}
+  if(document.getElementById("selll").value=="ment2"){document.getElementById("dos-ans").style.display = "inline";}
+
+}
+//document.getElementById("sel").addEventListener("click", las);
+//document.getElementById("sel").addEventListener('touchend', las);
+var read = [];
+var totes = [];
+var reed = [];
+var temp = "";
+
+function checkName(){
+    
+  var tempr = "Tweets from ";
+  if(document.getElementById("sel").value=="user" && document.getElementById("next").value==""){swal("Please fill out this filter before moving on 😊");return false;}
+  if(document.getElementById("sel").value=="user"){temp+="from:"+document.getElementById("next").value + " "; tempr+= "@"+document.getElementById("next").value +" "}
+  if(document.getElementById("next").value.charAt(0)=="@"){
+    temp = "from:" + document.getElementById("next").value.substr(1) + " ";
+    tempr = document.getElementById("next").value + " ";
+  }
+  if(document.getElementById("sel").value=="list"){temp+="list:"+document.getElementById("next").value + " "; tempr+= "list "+document.getElementById("next").value +" "}
+  if(document.getElementById("sel").value=="verified"){temp+="filter:verified" + " "; tempr+="verified accounts "}
+  if(document.getElementById("sel").value=="any"){tempr+="any account "}
+  if(document.getElementById("sell").value=="rt1"){temp+="min_retweets:"+document.getElementById("uno-ans").value + " "; tempr+="which have retweets more than " + document.getElementById("uno-ans").value + " "}
+  if(document.getElementById("sell").value=="reply1"){temp+="min_replies:"+document.getElementById("uno-ans").value + " "; tempr+="which have replies more than " + document.getElementById("uno-ans").value + " "}
+  if(document.getElementById("sell").value=="fave1"){temp+="min_faves:"+document.getElementById("uno-ans").value + " "; tempr+="which have likes more than " + document.getElementById("uno-ans").value + " "}
+  if(document.getElementById("sell").value=="ment1"){temp+=""+document.getElementById("uno-ans").value + " "; tempr+="which mention " + document.getElementById("uno-ans").value + " "}
+  if(document.getElementById("sell").value=="near1"){temp+="geocode:"+document.getElementById("uno-ans").value + ",25mi "; tempr+="which are near co-ordinates " + document.getElementById("uno-ans").value + " "}
+  if(document.getElementById("sell").value=="news1"){temp+="filter:news" + " "; tempr+="which are classified as news "}
+  if(document.getElementById("sell").value=="safe1"){temp+="filter:safe " + " "; tempr+="which are 'safe' "}
+  if(document.getElementById("sell").value=="img1"){temp+="filter:images " + " "; tempr+="which include images "}
+  if(document.getElementById("sell").value=="nrt1"){temp+="-RT" + " "; tempr+="which are not retweets"}
+  if(document.getElementById("selll").value=="rt2"){temp+="min_retweets:"+document.getElementById("dos-ans").value; tempr+="and have retweets more than " + document.getElementById("dos-ans").value}
+  if(document.getElementById("selll").value=="reply2"){temp+="min_replies:"+document.getElementById("dos-ans").value; tempr+="and have replies more than " + document.getElementById("dos-ans").value}
+  if(document.getElementById("selll").value=="fave2"){temp+="min_faves:"+document.getElementById("dos-ans").value; tempr+="and have likes more than " + document.getElementById("dos-ans").value}
+  if(document.getElementById("selll").value=="near2"){temp+="geocode:"+document.getElementById("dos-ans").value + ",25mi "; tempr+="which are near co-ordinates " + document.getElementById("uno-ans").value + " "}
+
+  if(document.getElementById("selll").value=="ment2"){temp+=""+document.getElementById("dos-ans").value; tempr+="and mention " + document.getElementById("dos-ans").value}
+  if(document.getElementById("selll").value=="news2"){temp+="filter:news";tempr+="and are classified as news"}
+  if(document.getElementById("selll").value=="safe2"){temp+="filter:safe"; tempr+="and are 'safe'"}
+  if(document.getElementById("selll").value=="img2"){temp+="filter:images" ; tempr+="and include images"}
+  if(document.getElementById("selll").value=="nrt2"){temp+="-RT"; tempr+="and are not retweets"}
+  totes.push(temp);
+  return true;
+} 
+
+jQuery("#form").submit(function(e) {
+     var self = this;
+     e.preventDefault();
+    if(checkName()){
+    console.log("addmyfilter (" + temp + ")");
+    window.location.href = "https://twitter.com/share?text=addmyfilter (" + temp.trim() + ")";
+  }
+}
+);
--- server.js
+++ server.js
@@ -4,60 +4,117 @@ const db = new JSONdb('db.json');
 
 var path = require('path'),
     express = require('express'),
-    app = express(),   
+    app = express(),
     Twit = require('twit'),
     config = {
-    /* Be sure to update the .env file with your API keys. See how to get them: https://botwiki.org/tutorials/how-to-create-a-twitter-app */      
-      twitter: {
-        consumer_key: process.env.CONSUMER_KEY,
-        consumer_secret: process.env.CONSUMER_SECRET,
-        access_token: process.env.ACCESS_TOKEN,
-        access_token_secret: process.env.ACCESS_TOKEN_SECRET
-      }
+        /* Be sure to update the .env file with your API keys. See how to get them: https://botwiki.org/tutorials/how-to-create-a-twitter-app */
+        twitter: {
+            consumer_key: process.env.CONSUMER_KEY,
+            consumer_secret: process.env.CONSUMER_SECRET,
+            access_token: process.env.ACCESS_TOKEN,
+            access_token_secret: process.env.ACCESS_TOKEN_SECRET
+        }
     },
     T = new Twit(config.twitter);
+//           T.get('search/tweets', { q: "from:producthunt", count: 1, result_type: 'recent'}, function(err, data, response) {
+//           if (err){
+//             console.log('Error!');
+//           }
+//           else{
+//             console.log(data["statuses"][0]);
+
+//           }})
 var queries = db.get('queries')
 console.log(queries)
 app.use(express.static('public'));
-/* You can use uptimerobot.com or a similar site to hit your /BOT_ENDPOINT to wake up your app and make your Twitter bot tweet. */
-app.all("/" + process.env.BOT_ENDPOINT, function (request, response) {
-/* The example below tweets out "Hello world!". */
-  var resp = response;
-  for(var  i = 0; i<=queries.length; i++){
-      console.log(i)
-      console.log(queries[i])
-      //anyone here to help? lemme see - cool -
-          T.get('search/tweets', { q: queries[i], count: 1, result_type: 'recent'}, function(err, data, response) {
-          if (err){
-            console.log("from: " + i)
-            console.log(queries)
-            return resp.sendStatus(500);
-            console.log('Error!');
-          }
-          else{
-            console.log(data["statuses"][0]["id_str"]);
-            T.post('statuses/retweet/:id', { id: data["statuses"][0]["id_str"] }, function (err, data, response) {
-              console.log(data)
-              
-              if (i == queries.length) return resp.sendStatus(200); // So it only responds at the end of the loop
-            
+
+function add(text, id) {
+    var faves;
+    T.get('statuses/show/:id', {
+        id: id
+    }, function(err, data, response) {
+        console.log(data["favorite_count"])
+        if (data["favorite_count"] > process.env.THRESHOLD) {
+            queries.push(text.slice(text.indexOf('(') + 1, text.indexOf(')')));
+            T.post('favorites/create', {
+                id: id
+            }, function(err, data, response) {
+                console.log(data)
             })
+            console.log(queries)
+            db.set('queries', queries);
+        }
+      else{
+        var params = {
+            status: "Time's up 🕒, and looks like you don't have enough favorites!",
+            in_reply_to_status_id: id,
+            auto_populate_reply_metadata: true
+        }
 
-          }})
-        
-                }})
-var stream = T.stream('statuses/filter', { track: 'addmyfilter', language: 'en' })
-stream.on('tweet', function (tweet) {
-  console.log(tweet)
-
-    var text = tweet["text"]
-    queries.push(text.slice(text.indexOf('(') +1,text.indexOf(')')));
-    T.post('favorites/create', {id: tweet["id_str"]}, function(err, data, response) {
-      console.log(data)
+        T.post('statuses/update', params, function(err, data, response) {
+            console.log(data)
+        })
+      }
     })
-  console.log(queries)
-  db.set('queries', queries);
+
+}
+
+/* You can use uptimerobot.com or a similar site to hit your /BOT_ENDPOINT to wake up your app and make your Twitter bot tweet. */
+app.all("/" + process.env.BOT_ENDPOINT, function(request, response) {
+    /* The example below tweets out "Hello world!". */
+    var resp = response;
+    for (var i = 0; i <= queries.length; i++) {
+        console.log(i)
+        console.log(queries[i])
+        //anyone here to help? lemme see - cool -
+        T.get('search/tweets', {
+            q: queries[i],
+            count: 1,
+            result_type: 'recent'
+        }, function(err, data, response) {
+            if (err) {
+                console.log("from: " + i)
+                console.log(queries)
+                return resp.sendStatus(500);
+                console.log('Error!');
+            } else {
+                console.log(data["statuses"][0]["id_str"]);
+                T.post('statuses/retweet/:id', {
+                    id: data["statuses"][0]["id_str"]
+                }, function(err, data, response) {
+                    console.log(data)
+
+                    if (i == queries.length) return resp.sendStatus(200); // So it only responds at the end of the loop
+
+                })
+
+            }
+        })
+    }
 })
-var listener = app.listen(process.env.PORT, function () {
-  console.log('Your bot is running on port ' + listener.address().port);
+var stream = T.stream('statuses/filter', {
+    track: 'addmyfilter'
+})
+stream.on('tweet', function(tweet) {
+    console.log(tweet)
+    var params = {
+        status: 'You have an hour to get '+ process.env.THRESHOLD +' favorites to your tweet, only then will your filter be added. Your time starts.....NOW! 🕒',
+        in_reply_to_status_id: tweet["id_str"],
+        auto_populate_reply_metadata: true
+    }
+
+    T.post('statuses/update', params, function(err, data, response) {
+        console.log(data)
+        setTimeout(function() {
+            add(tweet["text"], tweet["id_str"])
+        }, 1000 * 60 * 30);
+    })
+})
+app.get("/:url", (req, res) => {
+    var url = req.params.url;
+    res.sendFile(__dirname + "/public/" + url + ".html");
+})
+
+var listener = app.listen(process.env.PORT, function() {
+    console.log('Your bot is running on port ' + listener.address().port);
 });
